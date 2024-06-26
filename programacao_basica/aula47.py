a0935a9968bec998fc45be6580c1754cddb666c5
Osvaldo Alves <osvaldoalvesbrito789@gmail.com>
2024-06-25 22:21:31 -0300

    commit
    atualização

--- programacao_basica/aula47.py
+++ programacao_basica/aula47.py
@@ -1,12 +1,14 @@
+import os
+palavra_secreta = 'melancia'
 
-palavra_secreta = 'melancia'.lower()
-palavra = ''
-tentativas = 0
+letras_acertadas = ''
+numero_de_tentativas = 0
 
 while True:
+    palavra_formada = ''
+    numero_de_tentativas += 1
 
-    letra_digitada = input('Digite uma letra: ').lower()
-    tentativas += 1
+    letra_digitada = input('Digite uma letra: ')
 
     if len(letra_digitada) > 1:
         print('Digite apenas uma letra!\n')
@@ -16,12 +18,25 @@ while True:
         print('Você não digitou nada!\n')
         continue
 
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+
     for letra_secreta in palavra_secreta:
-        if letra_digitada == letra_secreta:
-            palavra += letra_digitada
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
         else:
-            palavra += '*'
+            palavra_formada += '*'
+
+    print(f'palavra: {palavra_formada} , tentativas: {numero_de_tentativas}\n')
+
+
+    if palavra_formada == palavra_secreta:
+        os.system('clear') # limpa o terminal
+        print('Parabens, você ganhou!'
+            f'A palavra era {palavra_secreta}\n'
+            f'Total de tentativas: {numero_de_tentativas}\n\n')
         
+        letras_acertadas = ''
+        numero_de_tentativas = 0
 
 
-    print(f'palavra: {palavra} , tentativas: {tentativas}\n')
