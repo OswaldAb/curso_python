70fe81f6b87aa86cde4c75f0232b9964bf8f1ea8
Osvaldo Alves <osvaldoalvesbrito789@gmail.com>
2024-07-29 21:26:10 -0300

    commit

--- prograamacao_POO/aula140.py
+++ prograamacao_POO/aula140.py
@@ -1,4 +1,32 @@
 # Herança multipla
 
-# mixin = ?
+# mixins = Uma classe que não faz parte da familia
+
+class A:
+    ...
+    def quem_sou(self):
+        print('A')
+
+
+class B(A):
+    ...
+    # def quem_sou(self):
+        # print('B')
+
+
+class C(A):
+    ...
+    # def quem_sou(self):
+    #     print('C')
+
+
+class D(B, C):
+    ...
+    # def quem_sou(self):
+    #     print('D')
+
+d = D()
+d.quem_sou()
+
+print(D.mro())
 
---
+++ prograamacao_POO/aula141/log.py
@@ -0,0 +1,17 @@
+# Abstração
+
+class Log: # Classe abstrata
+    def log(self, msg):
+        raise NotImplementedError('Implemente o método log')
+
+
+class LogFileMixin(Log):
+    def log(self, msg):
+        print(msg)
+
+
+
+
+if __name__ == '__main__':
+    l = LogFileMixin()
+    l.log('Mensagem de log.')
