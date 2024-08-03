726cf934ef5752cfed4f47b909d380c15120cfd2
Osvaldo Alves <osvaldoalvesbrito789@gmail.com>
2024-08-03 19:31:28 -0300

    commit

--- prograamacao_POO/aula153.py
+++ prograamacao_POO/aula153.py
@@ -3,12 +3,15 @@
 
 class CallMe:
     def __init__(self, phone):
-        self.phone = phone
+        self.phone  = phone
 
-    def __call__(self):
+    def __call__(self, *args, **kwargs):
         print('Chamando: ', self.phone)
 
-    
-call_1 = CallMe(18997945262)
 
+
+call_1 = CallMe('9948596231')
+
+print(call_1)
+print('_' * 50 + '\n')
 call_1()
--- prograamacao_POO/aula154.py
+++ prograamacao_POO/aula154.py
@@ -1,19 +1,37 @@
 # Classes decoradoras (Decorator classes)
 
-def dobra(funcao):
-    def interno(*args, **kwargs):
-        print('Vou te decorar.')
-        result = funcao(*args, **kwargs)
-        print('Te decorei.')
-        return result * 2
-    return interno
+class Multiplicar:
+    def __init__(self, func):
+        self.func = func
+        self._multiplicador = 10
+
+    def __call__(self, *args):
+        result = self.func(*args)
+        return result * self._multiplicador
 
 
-@dobra
+@Multiplicar
 def soma(x, y):
     return x + y
 
-
-
-res = soma(2,3)
+res = soma(2, 2)
 print(res)
+
+
+
+
+
+# def dobra(funcao):
+#     def interno(*args, **kwargs):
+#         print('Vou te decorar.')
+#         result = funcao(*args, **kwargs)
+#         print('Te decorei.')
+#         return result * 2
+#     return interno
+
+
+# @dobra
+# def soma(x, y):
+#     return x + y
+
+# print(soma(2,3))
